dcd1762ac2d8be42a4f17ff61510228ba4e3080e
AniAvazneli <avazneli.ani@gmail.com>
2022-11-26 23:41:47 +0400

    add cards

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,14 +12,16 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className="absolute w-[50] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div
+      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+    >
       <img
         src="/search.svg"
-        alt="loop"
+        alt="search"
         className="w-[24px] h-[24px] object-contain"
       />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
         METAVERSUS
       </h2>
       <img
